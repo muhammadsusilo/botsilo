a629800d1b49ce7dea689441afbb25cb2d5a3b66
Muhammad Susilo <muhammadsusilo43@gmail.com>
2023-12-05 13:35:44 +0700

    belajar destructuring

--- script.js
+++ script.js
@@ -3,24 +3,69 @@ const pertanyaan = document.getElementById("pertanyaan");
 const jawaban = document.getElementById("jawaban");
 let init = 0;
 
-const bot = () => {
-  return ["Halo, Perkenalkan nama saya bot, siapa nama kamu"];
+const bot = (data) => {
+  return [
+    "Halo, Perkenalkan nama saya Silobot, siapa nama kamu",
+    `Halo ${data?.nama}, berapa umur kamu `,
+    `Umur saya ${data?.umur}, Sekarang kuliah dimana??`,
+    `Kuliah di ${data?.kuliah},Jurusannya apa mas?`,
+    `Jurusannya ${data?.jurusan}, Semangat belajarnya`,
+  ];
 };
 
 pertanyaan.innerHTML = bot()[0];
+
 buttonKlik.addEventListener("click", function () {
   init++;
   if (init === 1) {
-    console.info({ nama: jawaban.value });
+    botDelay({ nama: jawaban.value });
   } else if (init === 2) {
-    console.info({ Kuliah: jawaban.value });
+    botDelay({ umur: jawaban.value });
   } else if (init === 3) {
-    console.info({ TTL: jawaban.value });
-  } else {
+    botDelay({ kuliah: jawaban.value });
+  } else if (init === 4) {
+    botDelay({ jurusan: jawaban.value });
+  } else if (init === 5) {
     bot2();
+  } else {
+    putar();
   }
 });
 
+// buttonKlik.addEventListener("click", function () {
+//   init++;
+//   switch (init) {
+//     case "  === 1":
+//       botDelay({ nama: jawaban.value });
+//       break;
+//     case "  === 1 ":
+//       botDelay({ umur: jawaban.value });
+//       break;
+//     case "  === 1 ":
+//       botDelay({ kuliah: jawaban.value });
+//       break;
+//     case "  === 1 ":
+//       botDelay({ jurusan: jawaban.value });
+//       break;
+//     case "  === 1 ":
+//       bot2();
+//       break;
+//     default:
+//       putar();
+//   }
+// });
+
+function botDelay(jawabanUser) {
+  setTimeout(() => {
+    pertanyaan.innerHTML = bot(jawabanUser)[init];
+  }, [1500]);
+  jawaban.value = "";
+}
+
 function bot2() {
-  console.info("terima kasih telah bermain");
+  pertanyaan.innerHTML = "Yaudah, terima kasih telah bermain di Silobot";
+}
+
+function putar() {
+  window.location.reload();
 }
